2827bd50019a6b9a553597d56d4968ff1baab782
Val-Del <84442853+Val-Del@users.noreply.github.com>
2023-01-13 21:03:14 +0100

    if elif else devinne

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 def main():
-    a = float(input("a"))
-    b = float(input("b"))
-    addition(a, b)
+    nombre = float(input("nombre "))
+    devinne(nombre)
+    # a = float(input("a"))
+    # b = float(input("b"))
+    # addition(a, b)
     # hi()
 
 
-def addition(a, b):
-    print(a + b)
+def devinne(nb):
+    rdm = 55
+    if nb < rdm :
+        print("inférieur")
+    elif nb > rdm :
+        print("supérieur")
+    else:
+        print("win")
+
+
+#
+# def addition(a, b):
+#     print(a + b)
 
 
 # def hi():
